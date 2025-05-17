8bd826eca1c8c604a086e1996c14b5f3728de0f0
phemcodejay <phemcode@live.com>
2025-05-18 01:03:57 +0300

    UP

--- pages/analytics/analytics-report.php
+++ pages/analytics/analytics-report.php
@@ -1,11 +1,5 @@
 <?php
-session_start([
-    'cookie_lifetime' => 86400,
-    'cookie_secure'   => true,
-    'cookie_httponly' => true,
-    'use_strict_mode' => true,
-    'sid_length'      => 48,
-]);
+session_start([]);
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 require_once __DIR__ . '/../../config/config.php'; // Includes database connection
